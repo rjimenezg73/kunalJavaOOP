720f273d78698253f8f3cd7d8975f5d82e64ae1b
rjimenezg73 <robertojimenezg@gmail.com>
2023-05-09 15:00:30 -0600

    OOP 1 | Introduction & Concepts - Classes, Objects, Constructors, Keywords - Part 02

--- Code/src/com/kunal/introduction/Main.java
+++ Code/src/com/kunal/introduction/Main.java
@@ -4,6 +4,9 @@ public class Main {
   public static void main(String[] args) {
 
     Student kunal = new Student();
+    Student rahul = new Student();
+    Student roberto = new Student(50, "Roberto", 66.6f);
+
     System.out.println(kunal);
 
     System.out.println(kunal.rno);
@@ -18,6 +21,16 @@ public class Main {
     System.out.println(kunal.name);
     System.out.println(kunal.marks);
 
+    System.out.println("---------------");
+    System.out.println(rahul.rno);
+    System.out.println(rahul.name);
+    System.out.println(rahul.marks);
+
+    System.out.println("---------------");
+    System.out.println(roberto.rno);
+    System.out.println(roberto.name);
+    System.out.println(roberto.marks);
+
   }
 }
 
@@ -27,4 +40,19 @@ class Student{
   int rno;
   String name;
   float marks;
+
+  // We need a way to add the values of the above properties object by object
+  // We need one word to access every object (this)
+  Student(){
+    this.rno = 15;
+    this.name = "Rahul";
+    this.marks = 99.9f;
+  }
+
+  Student(int rno, String name, float marks){
+    this.rno = rno;
+    this.name = name;
+    this.marks = marks;
+  }
+
 }

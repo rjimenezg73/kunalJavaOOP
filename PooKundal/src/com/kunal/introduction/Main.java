1e2e6aafb4777d4d0ce24a6f0fa1e51f33de30b4
rjimenezg73 <robertojimenezg@gmail.com>
2023-06-21 23:55:06 -0600

    Main demo class

---
+++ PooKundal/src/com/kunal/introduction/Main.java
@@ -0,0 +1,16 @@
+package com.kunal.introduction;
+
+public class Main {
+   public static void main(String[] args) {
+      // store 5 rolls numbers
+      int[] numbers = new int[5];
+
+      // store 5 names
+      String[] names = new String[5];
+
+      // data of 5 students: {roll no, name, marks}
+      int[] rno = new int[5];
+      String[] name = new String[5];
+      float[] marks = new float[5];
+   }
+}
